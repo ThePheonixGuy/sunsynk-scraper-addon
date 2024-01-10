ab05026b3a5502d28577ee69c5496bd89dba5199
Kyle Hendry <kylesh01@gmail.com>
2024-01-10 12:45:11 +0200

    Fix Syntax error

--- sunsynk-scraper/endpoints.py
+++ sunsynk-scraper/endpoints.py
@@ -24,7 +24,7 @@ def get_month_readings_endpoint(plant_id, date):
     energy_base_url = energy_base_url_1 if REGION == 1 else energy_base_url_2
     return f'{energy_base_url}{plant_id}/{month_readings_endpoint}&date={date.strftime("%Y-%m")}&id={plant_id}'
 
-def get_login_endpoint()
+def get_login_endpoint():
     login_endpoint = login_endpoint_1 if REGION == 1 else login_endpoint_2
     return login_endpoint
 
